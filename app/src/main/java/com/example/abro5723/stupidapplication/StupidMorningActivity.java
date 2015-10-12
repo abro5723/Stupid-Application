5d09233c2650318914172314ffb7bf4e704e0e07
abro5723 <abro5723@740s-pl-114.local>
2015-10-12 09:05:47 -0600

    First dumb project in Android Studio

--- app/src/main/java/com/example/abro5723/stupidapplication/StupidMorningActivity.java
+++ app/src/main/java/com/example/abro5723/stupidapplication/StupidMorningActivity.java
@@ -41,6 +41,7 @@ public class StupidMorningActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        // as you specify a parent activity in A+ndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
